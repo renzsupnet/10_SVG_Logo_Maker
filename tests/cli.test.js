0ba8c5a76f8fba4d3ceeccbae3c7e5ef3d4f3637
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2024-06-10 22:19:06 -0700

    Updated tests for cli

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,1 +1,30 @@
 const CLI = require('../lib/cli');
+
+describe('CLI', () => {
+    describe('Length Error Handling', () => {
+        it('should return true', () => {
+            const cli = new CLI();
+            const text = "ABC";
+            expect(cli.checkLength(text)).toBe(true);
+        }),
+        it('should return Letters must be at most 3 in length, please try again!', () => {
+            const cli = new CLI();
+            const text = "ABCD";
+            const result = "Letters must be at most 3 in length, please try again!";
+            expect(cli.checkLength(text)).toBe(result);
+        })
+    })
+    describe('checkValidColor Method', () => {
+        it('should return true', () => {
+            const cli = new CLI();
+            const color = 'red'
+            expect(cli.checkValidColor(color)).toBe(true);
+        }),
+        it('should return Invalid color please try again!', () => {
+            const cli = new CLI();
+            const color = 'Hello'
+            result = "Invalid color please try again!";
+            expect(cli.checkValidColor(color)).toBe(result);
+        })
+    })
+  });
